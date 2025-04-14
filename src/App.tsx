498c93d8402a8c214f9b7f47d89a748f5c2a8e71
Ura20 <ura2091.212@gmail.com>
2025-04-14 20:53:08 +0900

    App.tsxのエラー解消

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,11 @@ import { BrowserRouter } from "react-router-dom";
 import { Router } from "./router/Router";
 
 export default function App() {
-<<<<<<< HEAD
-    return (
-        <ChakraProvider>
-            <BrowserRouter>
-                <Router />
-            </BrowserRouter>
-        </ChakraProvider>
-    );
-=======
   return (
     <ChakraProvider>
       <BrowserRouter>
-        <Router />
+        <Router/>
       </BrowserRouter>
     </ChakraProvider>
-  );
->>>>>>> origin/feature/login-style
+  )
 }
