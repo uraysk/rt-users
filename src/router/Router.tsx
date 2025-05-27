6329cf468e4fe685a241df6cc8cf98164fb82bce
Ura20 <ura2091.212@gmail.com>
2025-05-27 12:41:02 +0900

    refact:Router.tsxのESLintエラーを修正しました

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,8 +2,9 @@ import { Route, Switch } from "react-router-dom";
 import { Login } from "../assets/components/pages/Login";
 import { HomeRoutes } from "./HomeRoutes";
 import { HeaderLayout } from "../assets/components/templates/HeaderLayout";
+import { VFC } from "react";
 
-export const Router = () => {
+export const Router:VFC = () => {
   return (
     <Switch>
       <Route exact path="/">
